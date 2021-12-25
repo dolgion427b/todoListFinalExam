af5997ca67416aa303c2f4b1478edb2fd3422324
ideree037 <se19d002@nmit.edu.mn>
2021-12-25 23:00:00 +0800

    commit of the last exam

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const { Console } = require("console");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient
+const app = express();
+
+app.listen(3001, function() {
+    console.log('Starting server at port 3001')
+});
+class toDoList {
+    constructor(_id, taskTitle, insertdate, isCompleted){
+        this._id = id;
+        this.taskTitle = taskTitle;
+        this.insertdate = insertdate;
+        this.isCompleted = isCompleted;
+    } 
+}
+
+const uri = "mongodb+srv://dolgion:TheWave247@cluster0.xauz4.mongodb.net/todoList?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const collection = client.db("todoList").collection("Lists");
+
+// Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static('views'));
+app.use(express.static('/style.css'));
+app.use(express.static('/'));
+app.use('/style.css', express.static(__dirname + '/style.css'));
+app.use("/script.js", express.static(__dirname + "/script.js"))
+
+app.get("/", function(req, res) {
+    const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+    let list_arr = [];
+    client.connect((error, result) => {
+        const collection = client.db("todoList").collection("Lists");
+        collection.find().toArray().then( result => {
+            list_arr = result.slice();
+        }).catch(error => console.error(error))
+    });
+    
+    console.log(list_arr);
+    client.close();
+    res.render('index', {List: list_arr});
+    //res.sendFile(__dirname + "/home.html");
+});
+app.get("/home", function(req,res) {
+    res.sendFile(__dirname + "/home.html")
+});
+app.post("/", function(req, res) {
+    var _taskTitle = "Task 1"
+    var _insertDate = Date.now()
+    var _isCompleted = "1"
+
+    client.connect((error, result ) =>{
+        const collection = client.db("todoList").collection("Lists");
+        collection.insertOne({id: 1, taskTitle: "myTask1", insertdate: Date.now(), isCompleted: true}).then(result => { console.log(result)}).catch(error=>console.error(error));
+        client.close();
+    })
+
+});
+
